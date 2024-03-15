ca81b6854af21fbdf2e12e830941f458d0ba500c
Pradeepkumarreddy Chama <139769887+cpkr162@users.noreply.github.com>
2024-03-15 17:02:15 +0530

    Create multiple-blocks.tf

---
+++ multiple-blocks.tf
@@ -0,0 +1,11 @@
+output "firstoutputblock" {
+        value = "This is first block"
+}
+
+output "secondoutputblock" {
+        value = "This is second block"
+}
+
+output "thirdoutputblock" {
+        value = "This is third block"
+}
